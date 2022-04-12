9b5c14ca33950696b61c6360bb4033b293544c64
code-ex0 <louis.sasse@protonmail.com>
2022-04-12 11:29:35 +0200

    fix(change user db)

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable, :trackable
+  :recoverable, :rememberable, :validatable, :confirmable, :trackable
+
+  has_many :tweets
+  has_many :favorites
+
+
+  has_many :followed_users, foreign_key: :follower_id, class_name: "Following"
+
+  has_many :followees, through: :followed_users, source: :followee
+
+  has_many :follower_users, foreign_key: :followed_id, class_name: "Following"
+
+  has_many :folowers, through: :follower_users, source: :follower
 end
