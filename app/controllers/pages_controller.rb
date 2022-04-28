835464cb303e66c5306c4484d923ea9223131a7d
xyo-mt <rivieremorgan99@gmail.com>
2022-04-28 10:50:34 +0200

    refactor(controllers): pages
    change index to page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,19 @@
+class PagesController < ApplicationController
+  # skip_before_action :authenticate_user! , only: [:home]
+
+  def home
+    @tweet = Tweet.new
+    @user = current_user
+    if current_user.nil?
+      @tweets = Tweet.all.order("created_at")
+    else
+      @tweets = []
+      current_user.followees.each do |follower|
+        follower.tweets.each do |tweet|
+          @tweets << tweet
+        end
+      end
+      @tweets = @tweets.sort_by { |tweet| tweet.created_at}.reverse!
+    end
+  end
+end
